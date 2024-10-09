909a13f43f4e1da192cefe631086c4db59f5abf9
Molok_cv58 <Molok_cv58@2-8>
2024-10-09 14:15:42 +0300

    Add project files.

---
+++ hwMultiFailProject/point.h
@@ -0,0 +1,14 @@
+#pragma once
+class point
+{
+		int x;
+		int y;
+		int z;
+	public:
+		point();
+		point(int x1, int y1, int z1);
+		void Init();
+		void Init(int x1, int y1, int z1);
+		void Output();
+};
+
